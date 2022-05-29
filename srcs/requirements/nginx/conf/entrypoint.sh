99645651b70bd947c07061d6ca4123e77c4d7055
yassine zemmouri <umpzemmouriy@gmail.com>
2022-05-29 19:48:30 +0100

    nginx

---
+++ srcs/requirements/nginx/conf/entrypoint.sh
@@ -0,0 +1,3 @@
+openssl req -x509 -nodes -days 365 -subj "/C=CA/ST=QC/O=Company, Inc./CN=mydomain.com" -addext "subjectAltName=DNS:mydomain.com" -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt;
+# Needed to make sure nginx is running after the commands are run
+nginx -g 'daemon off;'; nginx -s reload;
